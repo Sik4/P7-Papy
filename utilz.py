ec8218dbb99b234b9dde14cd46666a89445f27ed
Sik4 <cam.maury@gmail.com>
2019-02-19 06:51:51 +0100

    tried commit am, added stopwordsfr.json

---
+++ utilz.py
@@ -0,0 +1,9 @@
+
+def parser(self):
+    stripped_phrase = self.replace(" ", "").replace("-", "").replace("_", "")
+    print(stripped_phrase, type(stripped_phrase))
+    return stripped_phrase
+
+# def Grandpy(uinput):
+#  mettre la fonction à l'interieur
+
